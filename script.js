74269939fa089c1a0f5dc9473e0ed70865cece96
tatas-creativity <tatas.creativity@gmail.com>
2022-03-15 16:43:33 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let burger = document.querySelector('.burger');
+let navbar = document.querySelector('.section-of-products-navbar-wrapper');
+
+// burger.addEventListener ('click', () => {
+//     navbar.classList.toggle('display-navbar');
+// })
+
+// navbar.addEventListener;
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('display-navbar');
+    console.log('gg');
+})
